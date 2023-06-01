d82c4bc4e8cb042452aa77cb15807cacffa1e540
jayree <jayree.git@icloud.com>
2023-06-01 02:12:57 +0200

    chore: update patch files

--- lib/commands/jayree/org/get/settings.d.ts
+++ lib/commands/jayree/org/get/settings.d.ts
@@ -4,10 +4,10 @@ export default class ScratchOrgSettings extends SfCommand<AnyJson> {
     static readonly summary: string;
     static readonly examples: string[];
     static readonly flags: {
-        'target-org': import("@oclif/core/lib/interfaces").OptionFlag<import("@salesforce/core").Org, import("@oclif/core/lib/interfaces/parser").CustomOptions>;
-        'api-version': import("@oclif/core/lib/interfaces").OptionFlag<string | undefined, import("@oclif/core/lib/interfaces/parser").CustomOptions>;
-        writetoprojectscratchdeffile: import("@oclif/core/lib/interfaces").BooleanFlag<boolean>;
-        file: import("@oclif/core/lib/interfaces").OptionFlag<string | undefined, import("@oclif/core/lib/interfaces/parser").CustomOptions>;
+        'target-org': import("@oclif/core/lib/interfaces/parser.js").OptionFlag<import("@salesforce/core").Org, import("@oclif/core/lib/interfaces/parser.js").CustomOptions>;
+        'api-version': import("@oclif/core/lib/interfaces/parser.js").OptionFlag<string | undefined, import("@oclif/core/lib/interfaces/parser.js").CustomOptions>;
+        writetoprojectscratchdeffile: import("@oclif/core/lib/interfaces/parser.js").BooleanFlag<boolean>;
+        file: import("@oclif/core/lib/interfaces/parser.js").OptionFlag<string | undefined, import("@oclif/core/lib/interfaces/parser.js").CustomOptions>;
     };
     static readonly requiresProject = true;
     static readonly deprecateAliases = true;
