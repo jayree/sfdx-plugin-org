99f45241b0d8aede92f2713993b4e43a550ac6f7
jayree <jayree.git@icloud.com>
2023-11-17 09:39:16 +0100

    chore: fix

--- src/utils/puppeteer/statetasks.ts
+++ src/utils/puppeteer/statetasks.ts
@@ -5,7 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import playwright from 'playwright-chromium';
-import { Tabletojson as tabletojson } from 'tabletojson';
+import { tabletojson } from 'tabletojson';
 import Debug from 'debug';
 import { configSelectors, deactivateStates } from './countrystateconfig.js';
 import { readLaunchOptionsFromProject } from './utils.js';
